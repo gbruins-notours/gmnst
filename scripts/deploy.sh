6f824c2be05e1a3ed8afce700177318ec96f0203
greg bruins <gregbruins@gmail.com>
2017-09-16 16:51:17 -0700

    more CircleCI config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -100,7 +100,7 @@ selectNodeVersion () {
 
 echo Handling node.js deployment.
 # 1. Select node version
-#selectNodeVersion
+selectNodeVersion
 
 # 2. Install npm packages & build
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
