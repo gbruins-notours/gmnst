43302a9176afdb58083b734ae2043d1fee6f4f59
greg bruins <gregbruins@gmail.com>
2017-12-02 09:45:09 -0800

    updating travis

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,4 @@
 const cloneDeep = require('lodash.clonedeep');
-const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 let common = {
     client: 'postgres',
@@ -16,37 +15,29 @@ let common = {
     }
 };
 
-let dev = cloneDeep(common);
-// dev.connection = process.env.DATABASE_URL;
-dev.connection = {
+let commonDB = {
     host: process.env.DATA_DB_HOST,
     user: process.env.DATA_DB_USER,
     password: process.env.DATA_DB_PASS,
-    database: 'gonano'
-};
-
-let prod = cloneDeep(common);
-// prod.connection = {
-//     host: process.env.POSTGRESQLCONNSTR_POSTGRES_HOST,
-//     user: process.env.POSTGRESQLCONNSTR_POSTGRES_USER,
-//     password: process.env.POSTGRESQLCONNSTR_POSTGRES_PASSWORD,
-//     database: process.env.POSTGRESQLCONNSTR_POSTGRES_DB,
-//     port: 5432,
-//     ssl: true
-// };
-// prod.connection = `postgres://${process.env.POSTGRESQLCONNSTR_POSTGRES_USER}:${process.env.POSTGRESQLCONNSTR_POSTGRES_PASSWORD}@${process.env.POSTGRESQLCONNSTR_POSTGRES_HOST}:5432/${process.env.POSTGRESQLCONNSTR_POSTGRES_DB}?ssl=true`
-prod.connection = {
-    host: process.env.DATA_DB_HOST,
-    user: process.env.DATA_DB_USER,
-    password: process.env.DATA_DB_PASS,
-    database: 'gonano',
-    port: 5432,
-    ssl: true
-};
+    database: process.env.DATA_DB_NAME
+}
 
 let config = {
-    development: dev,
-    production: prod
+    development: null,
+    production: null
 };
 
+// DEVELOPMENT
+config.development = cloneDeep(common);
+config.development.connection = cloneDeep(commonDB);
+
+// PRODUCTION
+config.production = cloneDeep(common);
+config.production.connection = cloneDeep(commonDB);
+// Not sure if these are needed on nanobox so commenting out for now:
+// prod.connection.port = 5432;
+// prod.connection.ssl = true;
+
+
+let env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 module.exports = config[env];
