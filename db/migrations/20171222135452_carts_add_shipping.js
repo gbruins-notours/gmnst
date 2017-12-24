8035a2e557120e4321127e8221c4cf742c62daea
greg bruins <gregbruins@gmail.com>
2017-12-23 19:10:17 -0800

    updating knex scripts in package.json

--- db/migrations/20171222135452_carts_add_shipping.js
+++ db/migrations/20171222135452_carts_add_shipping.js
@@ -1,17 +1,13 @@
 const CoreService = require('../../server/plugins/core/core.service');
 
-module.exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
-            t.jsonb('shipping_rate').nullable();
-        })
-    ]);
+module.exports.up = function(knex) {
+    return knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
+        t.jsonb('shipping_rate').nullable();
+    })
 };
 
-module.exports.down = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
-            t.dropColumn('shipping_rate');
-        })
-    ]);
+module.exports.down = function(knex) {
+    return knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
+        t.dropColumn('shipping_rate');
+    })
 };
