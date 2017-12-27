d6f9563e1e862192aec66dbf81f268f87d812c4c
greg bruins <gregbruins@gmail.com>
2017-12-26 20:46:04 -0800

    adding postinstall script to package.json for DB migration

--- db/migrations/20171222223456_carts_drop_column_shipping_total.js
+++ db/migrations/20171222223456_carts_drop_column_shipping_total.js
@@ -1,13 +1,13 @@
 const CoreService = require('../../server/plugins/core/core.service');
 
 module.exports.up = function(knex) {
-    knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
+    return knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
         t.dropColumn('shipping_total');
     })
 };
 
 module.exports.down = function(knex) {
-    knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
+    return knex.schema.table(CoreService.DB_TABLES.carts, function(t) {
         t.decimal('shipping_total').nullable();
     })
 };
