9dcc5876100ace8ea773b9cb9104602f8221dcce
greg bruins <gregbruins@gmail.com>
2018-01-07 21:48:59 -0800

    more travis config updates for E2E

--- test/client/e2e/specs/test.spec.js
+++ test/client/e2e/specs/test.spec.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 describe('/', () => {
     it('should have title', () => {
         browser.url('/');
+        console.log("PAGE TITLE", browser.getTitle())
         assert.equal(browser.getTitle(), 'gmnst2');
     });
 });
