102a241b27495ed8a8b986cec16e16fb1896d106
greg bruins <gregbruins@gmail.com>
2018-01-07 18:14:11 -0800

    adding webdriverio for e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,37 @@ const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
-  root: true,
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  rules: {
-    'arrow-parens': OFF,
-    'brace-style': [ERROR, 'stroustrup'],
-    'camelcase': OFF,
-    'eol-last': OFF,
-    'indent': [ERROR, 4, { 'SwitchCase': 1 }],
-    'keyword-spacing': OFF,
-    'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
-    'no-multiple-empty-lines': OFF,
-    'no-trailing-spaces': OFF,
-    'operator-linebreak': OFF,
-    'padded-blocks': OFF,
-    'semi': OFF,
-    // Turned off spaces before pern because of broken Beautify formating for JS inside script tag
-    'space-before-function-paren': OFF,
-    'space-infix-ops': OFF,
-    'spaced-comment': OFF
-  }
+    root: true,
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    extends: 'standard',
+    // required to lint *.vue files
+    plugins: [
+        'html',
+        'mocha'
+    ],
+    env: {
+        mocha: true
+    },
+    globals: {
+        browser: false
+    },
+    rules: {
+        'mocha/no-exclusive-tests': 'error',
+        'arrow-parens': OFF,
+        'brace-style': [ERROR, 'stroustrup'],
+        'camelcase': OFF,
+        'eol-last': OFF,
+        'indent': [ERROR, 4, { 'SwitchCase': 1 }],
+        'keyword-spacing': OFF,
+        'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
+        'no-multiple-empty-lines': OFF,
+        'no-trailing-spaces': OFF,
+        'operator-linebreak': OFF,
+        'padded-blocks': OFF,
+        'semi': OFF,
+        // Turned off spaces before pern because of broken Beautify formating for JS inside script tag
+        'space-before-function-paren': OFF,
+        'space-infix-ops': OFF,
+        'spaced-comment': OFF
+    }
 };
--- test/client/e2e/runner.js
+++ test/client/e2e/runner.js
@@ -1,20 +1,17 @@
 'use strict';
 
+process.env.NODE_ENV = 'production'
+
 require('dotenv').config();
 
 const Hoek = require('hoek');
 const Path = require('path');
+const spawn = require('cross-spawn')
 const Server = require('../../../server');
 const manifest = require('../../../server/manifest');
 
-let srvr;
-
-// 1. start the dev server using production config
-process.env.NODE_ENV = 'test'
 
 Server.init(manifest, {relativeTo: Path.resolve(__dirname, '../../../server')}, (err, server) => {
-    srvr = server;
-
     if(err) {
         console.log('ERROR STARTING SERVER', err);
     }
@@ -23,33 +20,21 @@ Server.init(manifest, {relativeTo: Path.resolve(__dirname, '../../../server')}, 
     const api = server.select('api');
     console.log('API server started at: ' + api.info.uri);
 
-    // 2. run the nightwatch test suite against it
-    // to run in additional browsers:
-    //    1. add an entry in test/client/e2e/nightwatch.conf.json under "test_settings"
-    //    2. add it to the --env flag below
-    // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
-    // For more information on Nightwatch's config file, see
-    // http://nightwatchjs.org/guide#settings-file
-    let opts = process.argv.slice(2);
+    // let runner = spawn('./node_modules/.bin/wdio', ['./wdio.conf.js'], { stdio: 'inherit' })
+    let runner = spawn('./node_modules/.bin/wdio', ['./test/client/e2e/wdio.conf.js'], { stdio: 'inherit' })
 
-    if(opts.indexOf('--config') === -1) {
-        opts = opts.concat(['--config', 'test/client/e2e/nightwatch.conf.js'])
-    }
-
-    if(opts.indexOf('--env') === -1) {
-        opts = opts.concat(['--env', 'chrome'])
-    }
-
-    let spawn = require('cross-spawn')
-    let runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
+    runner.on('close', function (code) {
+        server.stop();
+        process.exit(code)
+    })
 
     runner.on('exit', function (code) {
-        srvr.stop();
+        server.stop();
         process.exit(code)
     })
 
     runner.on('error', function (err) {
-        srvr.stop();
+        server.stop();
         throw err
     });
 });
