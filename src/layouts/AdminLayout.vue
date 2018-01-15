d84f6b6b9ad48a78371760f305c3ec27760b594c
greg bruins <gregbruins@gmail.com>
2018-01-14 22:08:47 -0800

    adding new HeaderSidebarLayout

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,18 +1,16 @@
 <script>
 import Vue from 'vue'
-import { Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup } from 'element-ui'
+import { Menu, MenuItem } from 'element-ui'
+import HeaderSidebarLayout from '@/layouts/HeaderSidebarLayout'
 
-Vue.use(Container);
-Vue.use(Header);
-Vue.use(Aside);
-Vue.use(Main);
 Vue.use(Menu);
-Vue.use(Submenu);
 Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
-
 
 export default {
+    components: {
+        HeaderSidebarLayout
+    },
+
     methods: {
         goHome: function() {
             this.$router.push({ name: 'adminProducts' });
@@ -21,47 +19,43 @@ export default {
 }
 </script>
 
+
 <template>
-    <div class="layoutContainer">
-        <el-container>
-            <el-aside width="200px">
-                <el-menu
-                    :router="true"
-                    default-active="1"
-                    class="el-menu-vertical-demo"
-                    background-color="#304156"
-                    text-color="#fff"
-                    active-text-color="#ffd04b">
+    <header-sidebar-layout>
+        <template slot="sidebar">
+            <el-menu
+                :router="true"
+                default-active="1"
+                class="el-menu-vertical-demo"
+                background-color="#304156"
+                text-color="#fff"
+                active-text-color="#ffd04b">
 
-                    <el-menu-item :route="{ name: 'adminProducts' }" index="1">
-                        <i class="fa fa-cubes"></i>
-                        <span>Products</span>
-                    </el-menu-item>
+                <el-menu-item :route="{ name: 'adminProducts' }" index="1">
+                    <i class="fa fa-cubes"></i>
+                    <span>Products</span>
+                </el-menu-item>
 
-                    <el-menu-item :route="{ name: 'adminReports' }" index="2">
-                        <i class="fa fa-bar-chart"></i>
-                        <span>Reports2</span>
-                    </el-menu-item>
-                </el-menu>
-            </el-aside>
+                <el-menu-item :route="{ name: 'adminReports' }" index="2">
+                    <i class="fa fa-bar-chart"></i>
+                    <span>Reports2</span>
+                </el-menu-item>
+            </el-menu>
+        </template>
 
-            <el-container>
-                <el-header>
-                    <img class="Header-image cursorPointer" @click="goHome" src="/static/images/logo_header.png" alt="gmnst" />
-                </el-header>
+        <template slot="header">
+            <img class="Header-image cursorPointer" @click="goHome" src="/static/images/logo_header.png" alt="gmnst" />
+        </template>
 
-                <el-main>
-                    <slot></slot>
-                </el-main>
-            </el-container>
-        </el-container>
-    </div>
+        <template slot="main">
+            <slot></slot>
+        </template>
+    </header-sidebar-layout>
 </template>
 
 
 <style lang="scss">
     .layoutContainer {
-
         .el-menu {
             border: 0;
 
@@ -71,21 +65,9 @@ export default {
             }
         }
 
-        .el-aside {
-            background-color: #304156;
-            color: #fff;
-        }
-
-        .el-header {
-            background-color: #fff;
-            color: #333;
-            line-height: 60px;
-        }
-
         .Header-image {
             width: 135px;
             vertical-align: middle;
         }
-
     }
 </style>
---
+++ src/layouts/HeaderSidebarLayout.vue
@@ -0,0 +1,48 @@
+<script>
+import Vue from 'vue'
+import { Container, Header, Aside, Main } from 'element-ui'
+
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+
+export default {
+}
+</script>
+
+<template>
+    <div class="layoutContainer">
+        <el-container>
+            <el-aside width="200px">
+                <slot name="sidebar"></slot>
+            </el-aside>
+
+            <el-container>
+                <el-header>
+                    <slot name="header"></slot>
+                </el-header>
+
+                <el-main>
+                    <slot name="main"></slot>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+
+<style lang="scss">
+    .layoutContainer {
+        .el-aside {
+            background-color: #304156;
+            color: #fff;
+        }
+
+        .el-header {
+            background-color: #fff;
+            color: #333;
+            line-height: 60px;
+        }
+    }
+</style>
