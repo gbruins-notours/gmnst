d2f59c3b060817561f80c0b7f54fd0f8ac4fafea
Greg Bruins <gbruins@not-ours.com>
2018-02-20 21:29:46 -0800

    shopping cart - fixing bug where the featured pic was not displaying in the cart product list because the API was not returning any product relations (pics)

--- server/plugins/shopping-cart/models/ShoppingCart.js
+++ server/plugins/shopping-cart/models/ShoppingCart.js
@@ -120,6 +120,13 @@ module.exports = function (baseModel, bookshelf, server) {
                     withRelated: [
                         'cart_items.product', // https://stackoverflow.com/questions/35679855/always-fetch-from-related-models-in-bookshelf-js#35841710
                         {
+                            'cart_items.product.pics': (query) => {
+                                query.where('is_visible', '=', true);
+                                query.orderBy('sort_order', 'ASC');
+                                query.limit(1); 
+                            }
+                        },
+                        {
                             cart_items: (query) => {
                                 // Sorting by updated_at (instead of created_at) will keep the most recently
                                 // updated cart item at the top of the list, which I think is the most
