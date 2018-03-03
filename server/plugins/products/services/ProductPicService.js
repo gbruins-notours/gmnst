2d6763cf6463e15ae1d194951f4c90632d5abc0a
Greg Bruins <gbruins@not-ours.com>
2018-03-02 23:59:45 -0800

    admin - product list - fixing some issues with product pic variants

--- server/plugins/products/services/ProductPicService.js
+++ server/plugins/products/services/ProductPicService.js
@@ -13,21 +13,92 @@ const productDirectory = process.env.NODE_ENV === 'production'
     ? path.join(__dirname, '../../../../dist/static/images/product/') //TODO: is this the right path?
     : path.join(__dirname, '../../../../static/images/product/');
 
+const imageMimeTypeWhiteList = [
+    'image/png',
+    'image/gif',
+    'image/jpeg',
+    'image/pjpeg'
+];
 
-// TODO:
-// saveFIle - save the big file too
-// deleteFile - need promise based way of deleting the 2 files? (fs-extra npm module)
+
+function fileIsImage(fileData) {
+    let typeObj = fileType(fileData);
+
+    if(isObject(typeObj) && imageMimeTypeWhiteList.indexOf(typeObj.mime) > -1) {
+        return typeObj;
+    }
+
+    return false;
+}
+
+
+function deleteFile(fileName) {
+    return new Promise((resolve, reject) => {
+        if(!fileName) {
+            return reject('Can not delete file because fileName was not provided');
+        }
+
+        fs.unlink(productDirectory + fileName, (err) => {
+            if(err) {
+                return reject(err);
+            }
+
+            resolve()
+        });
+    });
+}
+
+
+/**
+ * Saves a new picture file
+ * The new file name will be returned in the response if the file save was successful
+ * otherwise the success response will be empty
+ */
+function resizeAndWrite(req, width) {
+    // Cloning is necessary because the file.pipe operation below seems
+    // to modify the request.payload.file value, causing subsequest
+    // resize attemtps on the same file to fail.
+    let request = cloneDeep(req);
+
+    return new Promise((resolve, reject) => {
+        if(request.payload.file) {
+            let typeObj = fileIsImage(request.payload.file._data)
+
+            if(typeObj) {
+                let w = parseInt(width, 10) || 600
+                let cleanId = helperService.stripTags(helperService.stripQuotes(request.payload.product_id));
+                let fileName = `${cleanId}_${new Date().getTime()}.${typeObj.ext}`;
+
+                let transformer = sharp()
+                    .resize(w)
+                    .max()
+                    .withoutEnlargement(true)
+                    .on('info', function(info) {
+                        info.file_name = fileName;
+                        return resolve(info);
+                    });
+
+                request.payload.file.pipe(transformer).pipe(
+                    fs.createWriteStream(productDirectory + fileName)
+                );
+            }
+            else {
+                global.logger.info('SAVING PRODUCT FAILED BECAUSE WRONG MIME TYPE');
+                return reject('File type must be one of: ' + imageMimeTypeWhiteList.join(','))
+            }
+        }  
+        else {
+            resolve();
+        }
+    });
+}
+
+
+
 module.exports = class ProductPicService {
 
     constructor(server) {
         this.server = server;
-
-        this.imageMimeTypeWhiteList = [
-            'image/png',
-            'image/gif',
-            'image/jpeg',
-            'image/pjpeg'
-        ];
     }
 
 
@@ -35,55 +106,9 @@ module.exports = class ProductPicService {
         return this.server.plugins.BookshelfOrm.bookshelf.model('ProductPic')
     }
 
+
     getProductPicVariantModel() {
         return this.server.plugins.BookshelfOrm.bookshelf.model('ProductPicVariant')
-    }
-   
-   
-    /**
-     * Saves a new picture file
-     * The new file name will be returned in the response if the file save was successful
-     * otherwise the success response will be empty
-     */
-    resizeAndWrite(req, width) {
-        let self = this;
-
-        // Cloning is necessary because the file.pipe operation below seems
-        // to modify the request.payload.file value, causing subsequest
-        // resize attemtps on the same file to fail.
-        let request = cloneDeep(req);
-
-        return new Promise((resolve, reject) => {
-            if(request.payload.file) {
-                let typeObj = self.fileIsImage(request.payload.file._data)
-
-                if(typeObj) {
-                    let w = parseInt(width, 10) || 600
-                    let cleanId = helperService.stripTags(helperService.stripQuotes(request.payload.product_id));
-                    let fileName = `${cleanId}_${new Date().getTime()}.${typeObj.ext}`;
-
-                    let transformer = sharp()
-                        .resize(w)
-                        .max()
-                        .withoutEnlargement(true)
-                        .on('info', function(info) {
-                            info.file_name = fileName;
-                            return resolve(info);
-                        });
-
-                    request.payload.file.pipe(transformer).pipe(
-                        fs.createWriteStream(productDirectory + fileName)
-                    );
-                }
-                else {
-                    global.logger.info('SAVING PRODUCT FAILED BECAUSE WRONG MIME TYPE');
-                    return reject('File type must be one of: ' + self.imageMimeTypeWhiteList.join(','))
-                }
-            }  
-            else {
-                resolve();
-            }
-        });
     }
 
 
@@ -138,25 +163,33 @@ module.exports = class ProductPicService {
                         return;
                     }
                     
-                    let fileName = ProductPic.get('file_name');
+                    let json = ProductPic.toJSON();
+                    global.logger.info('PRODUCT PIC WITH VARIANTS', json)
 
-                    global.logger.info('PRODUCT PIC WITH VARIANTS', ProductPic.toJSON())
-
-                    if(fileName) {
-                        // TODO: unlink the variant file too.  However would it be 
-                        // better to use a lib like fs-extra so the unlink returns
-                        // a promise instead of a callback?  Needs a way to remove the 2 files
-                        // asynchronously...not callbacks
-                        
-                        fs.unlink(productDirectory + fileName, (err) => {
-                            if(err) {
+                    if(json.file_name) {
+                        // Unlink the main product pic
+                        deleteFile(json.file_name)
+                            .then(() => {
+                                global.logger.info('PRODUCT PIC - FILE DELETED', json.file_name);
+                            })
+                            .catch((err) => {
                                 global.logger.error('PRODUCT PIC - ERROR DELETING FILE', err);
-                                return reject(err);
-                            }
+                            });
 
-                            global.logger.info('PRODUCT PIC - FILE DELETED', fileName);
-                            return resolve(ProductPic);
-                        });
+                        // Unlink the product pic variants
+                        if(Array.isArray(json.pic_variants)) {
+                            json.pic_variants.forEach((obj) => {
+                                deleteFile(obj.file_name)
+                                    .then(() => {
+                                        global.logger.info('PRODUCT PIC VARIANT - FILE DELETED', obj.file_name);
+                                    })
+                                    .catch((err) => {
+                                        global.logger.error('PRODUCT PIC VARIANT - ERROR DELETING FILE', err);
+                                    });
+                            });
+                        }
+
+                        return resolve(ProductPic);
                     }  
                     else {
                         return resolve(ProductPic);
@@ -167,17 +200,17 @@ module.exports = class ProductPicService {
 
 
     deleteProductPicVariants(ProductPic) {
-        var self = this;
+        let self = this;
 
         if(ProductPic) {
-            let variants = ProductPic.get('pic_variants');
+            let json = ProductPic.toJSON();
 
-            if(Array.isArray(variants)) {
-                variants.forEach((ProductPicVariant) => {
-                    global.logger.info('DELETING PRODUCT PIC VARIANT', ProductPicVariant.get('id'));
+            if(Array.isArray(json.pic_variants)) {
+                json.pic_variants.forEach((obj) => {
+                    global.logger.info('DELETING PRODUCT PIC VARIANT FROM DB', obj.id);
 
                     self.getProductPicVariantModel().destroy({
-                        id: ProductPicVariant.get('id')
+                        id: obj.id
                     })
                 });
             }
@@ -205,7 +238,7 @@ module.exports = class ProductPicService {
                 .then((ProductPic) => {
                     // Always delete the variants and re-create
                     self.deleteProductPicVariants(ProductPic);
-                    return self.resizeAndWrite(request, 600)
+                    return resizeAndWrite(request, 600)
                 })
                 .then((resizeResponse) => {
                     global.logger.info('PRODUCT PIC - FILE RESIZED (600)', resizeResponse);
@@ -233,7 +266,7 @@ module.exports = class ProductPicService {
                     global.logger.info('PRODUCT PIC UPSERTED', productPic.get('id'));
 
                     // ProductPicVariant:
-                    return self.resizeAndWrite(request, 1000)
+                    return resizeAndWrite(request, 1000)
                 })
                 .then((resizeResponse) => {
                     global.logger.info('PRODUCT PIC VARIANT - FILE RESIZED (1000)', resizeResponse);
@@ -262,21 +295,5 @@ module.exports = class ProductPicService {
                     return reject(err);
                 });
         });
-    }
-
-
-    /**
-     * Determines if the given file data is an appropriate mime type
-     * 
-     * @param {*} fileData  Most likely from request.payload.file._data
-     */
-    fileIsImage(fileData) {
-        let typeObj = fileType(fileData);
-
-        if(isObject(typeObj) && this.imageMimeTypeWhiteList.indexOf(typeObj.mime) > -1) {
-            return typeObj;
-        }
-
-        return false;
     }
 }
