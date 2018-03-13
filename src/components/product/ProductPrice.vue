4a2b6cc03652948488481907bdc0f302c98623e6
Greg Bruins <gbruins@not-ours.com>
2018-03-12 22:49:21 -0700

    adding strikethrough to product price if it's on sale

--- src/components/product/ProductPrice.vue
+++ src/components/product/ProductPrice.vue
@@ -10,14 +10,19 @@
         },
 
         computed: {
-            price: function() {
-                if (isObject(this.product)) {
-                    if (this.product.is_on_sale && this.product.sale_price) {
-                        return this.product.sale_price;
-                    }
-                    else if (this.product.base_price) {
-                        return this.product.base_price;
-                    }
+            basePrice: function() {
+                if (isObject(this.product) && this.product.base_price) {
+                    return this.$n(this.product.base_price, 'currency');
+                }
+
+                return 0;
+            },
+
+            salePrice: function() {
+                if (isObject(this.product)
+                    && this.product.is_on_sale 
+                    && this.product.sale_price) {
+                        return this.$n(this.product.sale_price, 'currency');
                 }
 
                 return 0;
@@ -28,5 +33,12 @@
 
 
 <template>
-    <span>{{ $n(price, 'currency') }}</span>
+    <div class="inlineBlock">
+        <div class="inlineBlock" v-if="salePrice && basePrice">
+            <div class="inlineBlock colorGrayLighter strikethrough mrs">{{ basePrice }}</div>
+            <div class="inlineBlock">{{ salePrice }}</div>
+        </div> 
+        <div v-else-if="salePrice" class="inlineBlock">{{ salePrice }}</div>
+        <div v-else class="inlineBlock">{{ basePrice }}</div>
+    </div>
 </template>
