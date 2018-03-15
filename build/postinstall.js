6346c49118c0cfd7dc2257d8e1881781ebbaefc6
greg bruins <gregbruins@gmail.com>
2018-03-14 22:05:01 -0700

    updating postinstall.js

--- build/postinstall.js
+++ build/postinstall.js
@@ -3,11 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 
-// adding write permissions to the product folder so 
-// product images can be uploaded/saved from the admin page.
+const permission = '755';
+
 if(process.env.NODE_ENV === 'production') {
+    // adding write permissions to the product folder so 
+    // product images can be uploaded/saved from the admin page.
     fs.chmodSync(
-        path.join(__dirname, '../dist/static/images/product/'), 
-        '644'
+        // path.join(__dirname, '../dist/static/images/product/'), 
+        '/app/dist/static/images/product',
+        permission
+    );
+
+    // for writing new log files:
+    fs.chmodSync(
+        '/app/dist/logs',
+        permission
     );
 }
